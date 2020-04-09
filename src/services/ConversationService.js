cf249d74a121b251dd3194c2abaa419d30dfa7ae
Tim Williams <jtimwill@gmail.com>
2020-04-08 20:53:25 -0400

    Correct URL

--- src/services/ConversationService.js
+++ src/services/ConversationService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = process.env.REACT_APP_API_URL  || 'http://localhost:4001';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL  || 'https://peaceful-tor-53707.herokuapp.com';
 
 export function callConversationHandler(name, current_service, new_service) {
   return axios.get(`/api/conversation-handler/?name=${name}&current_service=${current_service}&new_service=${new_service}`);
