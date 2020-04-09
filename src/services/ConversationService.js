92e8329bcfb78c45f19c179464bd9e0264d3fdf4
Tim Williams <jtimwill@gmail.com>
2020-04-08 20:38:07 -0400

    Upate URL

--- src/services/ConversationService.js
+++ src/services/ConversationService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4001/api/conversation-handler';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL  || 'http://localhost:4001';
 
 export function callConversationHandler(name, current_service, new_service) {
-  return axios.get(`/?name=${name}&current_service=${current_service}&new_service=${new_service}`);
+  return axios.get(`/api/conversation-handler/?name=${name}&current_service=${current_service}&new_service=${new_service}`);
 };
 
 export function getFirstState(name) {
-  return axios.get(`/first-state/?name=${name}`);
+  return axios.get(`/api/conversation-handler/first-state/?name=${name}`);
 };
